80643f84c44bdbb42bacd90e92422cf3a7e960be
tico <lours2021@gmail.com>
2021-03-05 11:47:30 +0900

    Initial Commit

---
+++ src/main/java/kr/tripamigo/tripamigo/TripamigoApplication.java
@@ -0,0 +1,13 @@
+package kr.tripamigo.tripamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripamigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripamigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/tripamigo/tripamigo/TripamigoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tripamigo.tripamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripamigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
