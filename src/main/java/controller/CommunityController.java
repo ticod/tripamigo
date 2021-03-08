3a5974f656381a321052b3816a927d186642031b
tico <lours2021@gmail.com>
2021-03-08 23:19:31 +0900

    추가: Controllers
    화면 별 기본 Controller 소스 작성

---
+++ src/main/java/controller/CommunityController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/community")
+public class CommunityController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        return "community/home";
+    }
+
+}
---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    String home(Model model) {
+        return "home";
+    }
+
+}
