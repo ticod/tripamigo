3bc47375aa7275b1c78aff78a4fef3ad0e47ccc0
tico <lours2021@gmail.com>
2021-03-10 09:48:08 +0900

    추가: UserRepository
    추후 다른 클래스를 사용하게 될 수 있으므로
    Repository와 JpaRepository 분리

---
+++ src/main/java/kr/tripamigo/tripamigo/repository/UserJpaRepository.java
@@ -0,0 +1,7 @@
+package kr.tripamigo.tripamigo.repository;
+
+import kr.tripamigo.tripamigo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends UserRepository, JpaRepository<User, Long> {
+}
