337c873472aeff8fa4064b0d43a144d02457ea54
tico <lours2021@gmail.com>
2021-03-15 00:11:54 +0900

    수정: MailSendUtil test
    admin controller에서 임시 테스트 코드

--- src/main/java/kr/tripamigo/tripamigo/controller/AdminController.java
+++ src/main/java/kr/tripamigo/tripamigo/controller/AdminController.java
@@ -1,12 +1,29 @@
 package kr.tripamigo.tripamigo.controller;
 
+import kr.tripamigo.tripamigo.util.MailSendUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private MailSendUtil mailSendUtil;
 
+    @RequestMapping("/mailtest")
+    @ResponseBody
+    public boolean testMail() {
+        String msg = "hello world";
+        try {
+            mailSendUtil.sendMail("lours2011@gmail.com", MailSendUtil.DEFAULT_FROM_ADDRESS, "tripamigo", msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
 }
