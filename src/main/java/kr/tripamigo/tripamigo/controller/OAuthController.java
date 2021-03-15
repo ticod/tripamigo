17ec5d6c7d00851260f353ea129000d4b8a0fbc3
tico <lours2021@gmail.com>
2021-03-15 14:57:34 +0900

    추가: OAuthController
    OAuth 인증용 컨트롤러 추가

---
+++ src/main/java/kr/tripamigo/tripamigo/controller/OAuthController.java
@@ -0,0 +1,35 @@
+package kr.tripamigo.tripamigo.controller;
+
+import kr.tripamigo.tripamigo.dto.OAuthKakaoInfoDTO;
+import kr.tripamigo.tripamigo.dto.OAuthTokenDTO;
+import kr.tripamigo.tripamigo.service.KakaoOAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/oauth")
+public class OAuthController {
+
+    @Autowired
+    private KakaoOAuthService kakaoOauthService;
+
+    @ResponseBody
+    @RequestMapping("/kakao_auth_code")
+    public String kakaoAuthCode() {
+        return kakaoOauthService.getAuthCodeURI();
+    }
+
+    @RequestMapping("/kakao_login")
+    public String kakaoLogin(@RequestParam("code") String code) {
+        OAuthTokenDTO tokens = kakaoOauthService.getTokens(code);
+        OAuthKakaoInfoDTO infoDTO = kakaoOauthService.getUserId(tokens.getAccessToken());
+        /*
+        TODO: 로그인 or 회원가입 처리
+         */
+        return "redirect:/home";
+    }
+
+}
