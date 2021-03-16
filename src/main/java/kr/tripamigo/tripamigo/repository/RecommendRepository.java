a89485c81bc1cc0fe11da8e4be138ae0515857d7
tico <lours2021@gmail.com>
2021-03-16 16:33:06 +0900

    추가: RecommendRepository & test
    RecommendRepository 메서드 추가
    없는 boardSeq를 활용하여 쿼리메서드 테스트

--- src/main/java/kr/tripamigo/tripamigo/repository/RecommendRepository.java
+++ src/main/java/kr/tripamigo/tripamigo/repository/RecommendRepository.java
@@ -1,9 +1,15 @@
 package kr.tripamigo.tripamigo.repository;
 
+import kr.tripamigo.tripamigo.domain.Recommend;
 import kr.tripamigo.tripamigo.domain.RecommendType;
+
+import java.util.List;
 
 public interface RecommendRepository {
 	
-	
-//	int countByRecommendTypeAndContentSeq(RecommendType type, int seq); 
+	Recommend save(Recommend recommend);
+	List<Recommend> findAll();
+	int countByRecommendTypeAndContentSeq(RecommendType recommendType, Long contentSeq);
+	int countByUserSeqAndStatus(Long userSeq, boolean b);
+
 }
---
+++ src/test/java/kr/tripamigo/tripamigo/repository/RecommendRepositoryTest.java
@@ -0,0 +1,60 @@
+package kr.tripamigo.tripamigo.repository;
+
+import kr.tripamigo.tripamigo.domain.Recommend;
+import kr.tripamigo.tripamigo.domain.RecommendType;
+import kr.tripamigo.tripamigo.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class RecommendRepositoryTest {
+
+    @Autowired RecommendRepository recommendRepository;
+    @Autowired UserRepository userRepository;
+
+    private List<User> users;
+
+    @BeforeEach
+    void before() {
+
+        users = userRepository.findAll();
+        Recommend recommend1 = new Recommend();
+        recommend1.setContentSeq(1L);
+        recommend1.setRecommendType(RecommendType.BOARD);
+        recommend1.setUserSeq(users.get(0).getUserSeq());
+        recommend1.setDate(LocalDateTime.now());
+        recommend1.setStatus(true);
+        recommendRepository.save(recommend1);
+
+
+        Recommend recommend2 = new Recommend();
+        recommend2.setContentSeq(1L);
+        recommend2.setRecommendType(RecommendType.BOARD);
+        recommend2.setUserSeq(users.get(1).getUserSeq());
+        recommend2.setDate(LocalDateTime.now());
+        recommend2.setStatus(true);
+        recommendRepository.save(recommend2);
+
+    }
+
+    @Test
+    void repositoryCountByRecommendTypeAndSeq() {
+        int count = recommendRepository.countByRecommendTypeAndContentSeq(RecommendType.BOARD, 1L);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    void userRecommendCount() {
+        int count = recommendRepository.countByUserSeqAndStatus(users.get(0).getUserSeq(), true);
+        Assertions.assertThat(count).isEqualTo(1);
+    }
+
+}
