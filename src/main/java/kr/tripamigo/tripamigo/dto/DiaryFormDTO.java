6c8caa89098e5f71f11474a362b17a88a5c8d384
hyun <dhtmdgussksm@naver.com>
2021-03-17 13:51:25 +0900

    1. my page 기본 템플 2. service, DTO, controller (수정중)

---
+++ src/main/java/kr/tripamigo/tripamigo/dto/DiaryFormDTO.java
@@ -0,0 +1,27 @@
+package kr.tripamigo.tripamigo.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+/*
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class DiaryFormDTO {
+
+	@NotBlank(message="제목을 입력하세요")
+    private String subject;
+	
+	@NotBlank(message="내용을 입력하세요")
+    private String content;
+	
+    private String tags;
+    
+    private String thumbnail;
+
+}
+*/
