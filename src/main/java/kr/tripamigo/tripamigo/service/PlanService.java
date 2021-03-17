a9422c3b6fab6e2d9f30f53e1281710c4aa5a175
tico <lours2021@gmail.com>
2021-03-17 21:35:08 +0900

    추가: PlanService method
    listAll & countAll 추가
    테스트 후 페이징 진행

--- src/main/java/kr/tripamigo/tripamigo/service/PlanService.java
+++ src/main/java/kr/tripamigo/tripamigo/service/PlanService.java
@@ -1,13 +1,24 @@
 package kr.tripamigo.tripamigo.service;
 
+import kr.tripamigo.tripamigo.domain.board.Plan;
 import kr.tripamigo.tripamigo.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 @RequiredArgsConstructor
 public class PlanService {
 
     private final PlanRepository planRepository;
+
+    public List<Plan> listAll() {
+        return planRepository.findAllByStatus(true);
+    }
+
+    public int countAll() {
+        return planRepository.countByStatus(true);
+    }
 
 }
