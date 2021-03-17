063e2877993cee3e490c21e870b1062086287f01
tico <lours2021@gmail.com>
2021-03-17 23:42:11 +0900

    추가: Controller mappings
    CRUD 매핑 설계

--- src/main/java/kr/tripamigo/tripamigo/controller/PlanController.java
+++ src/main/java/kr/tripamigo/tripamigo/controller/PlanController.java
@@ -1,10 +1,19 @@
 package kr.tripamigo.tripamigo.controller;
 
+import kr.tripamigo.tripamigo.domain.User;
+import kr.tripamigo.tripamigo.domain.board.Plan;
+import kr.tripamigo.tripamigo.dto.PlanFormDTO;
 import kr.tripamigo.tripamigo.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/community")
@@ -13,10 +22,49 @@ public class PlanController {
     @Autowired
     PlanService planService;
 
+    private final static String REDIRECT_HOME = "redirect:/community/plan";
+
+    /* Read */
     @RequestMapping("/plan")
     public String planHome(Model model) {
         model.addAttribute("planList", planService.listAll());
         return "/plan/list";
+    }
+
+    @GetMapping("/plan/{planId}")
+    public String planDetail(@PathVariable("planId") Long planId, Model model) {
+        model.addAttribute("plan", planService.getPlanBy(planId));
+        return "/plan/detail";
+    }
+
+    /* Create */
+    @GetMapping("/plan/write")
+    public String planWrite(PlanFormDTO planFormDTO, Model model) {
+        return "/plan/write";
+    }
+
+    @PostMapping("/plan/write")
+    public String planWrite(@Valid PlanFormDTO planFormDTO, Model model, HttpSession session) {
+        User loginUser = (User) session.getAttribute("loginUser");
+        planFormDTO.setUser(loginUser);
+        return REDIRECT_HOME;
+    }
+
+    /* Update */
+    @GetMapping("/plan/update/{planId}")
+    public String planUpdate(@PathVariable("planId") Long planId, Model model) {
+        return "/plan/update";
+    }
+
+    @PostMapping("/plan/update/{planId}")
+    public String planUpdate(@Valid PlanFormDTO planFormDTO, Model model, HttpSession session) {
+        return REDIRECT_HOME;
+    }
+
+    /* Delete */
+    @PostMapping("/plan/delete/{planId}")
+    public String planDelete(@PathVariable("planId") Long planId, Model model, HttpSession session) {
+        return REDIRECT_HOME;
     }
 
 }
