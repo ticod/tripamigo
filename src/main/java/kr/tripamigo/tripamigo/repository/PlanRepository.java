0d991483292a1a45d4b9c7a254606ab3887398e9
tico <lours2021@gmail.com>
2021-03-17 21:30:46 +0900

    추가: PlanRepository method
    findAll & count byStatus 추가

--- src/main/java/kr/tripamigo/tripamigo/repository/PlanRepository.java
+++ src/main/java/kr/tripamigo/tripamigo/repository/PlanRepository.java
@@ -1,4 +1,12 @@
 package kr.tripamigo.tripamigo.repository;
 
+import kr.tripamigo.tripamigo.domain.board.Plan;
+
+import java.util.List;
+
 public interface PlanRepository {
+
+    List<Plan> findAllByStatus(boolean status);
+    int countByStatus(boolean status);
+
 }
