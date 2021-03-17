95d1912f0abf337e9c705922a120ef84bc0cdb29
tico <lours2021@gmail.com>
2021-03-17 21:37:44 +0900

    추가: PlanController /plan
    add list page

--- src/main/java/kr/tripamigo/tripamigo/controller/PlanController.java
+++ src/main/java/kr/tripamigo/tripamigo/controller/PlanController.java
@@ -3,11 +3,20 @@ package kr.tripamigo.tripamigo.controller;
 import kr.tripamigo.tripamigo.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/community")
 public class PlanController {
 
     @Autowired
     PlanService planService;
+
+    @RequestMapping("/plan")
+    public String planHome(Model model) {
+        model.addAttribute(planService.listAll());
+        return "/plan/list";
+    }
 
 }
