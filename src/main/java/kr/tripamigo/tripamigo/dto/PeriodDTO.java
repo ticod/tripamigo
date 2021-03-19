057912229e86ea22a230cd52464635b2824d26a4
tico <lours2021@gmail.com>
2021-03-19 11:36:44 +0900

    수정: PeriodDTO 유효성 검사 추가

--- src/main/java/kr/tripamigo/tripamigo/dto/PeriodDTO.java
+++ src/main/java/kr/tripamigo/tripamigo/dto/PeriodDTO.java
@@ -2,6 +2,8 @@ package kr.tripamigo.tripamigo.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -10,14 +12,22 @@ import lombok.*;
 @Builder
 public class PeriodDTO {
 
+    @NotBlank(message = "연도를 입력해주세요")
     private int startYear;
+    @NotBlank(message = "월을 입력해주세요")
     private int startMonth;
+    @NotBlank(message = "일을 입력해주세요")
     private int startDay;
+    @NotBlank(message = "시간을 입력해주세요")
     private int startTime;
 
+    @NotBlank(message = "연도를 입력해주세요")
     private int endYear;
+    @NotBlank(message = "월을 입력해주세요")
     private int endMonth;
+    @NotBlank(message = "일을 입력해주세요")
     private int endDay;
+    @NotBlank(message = "시간을 입력해주세요")
     private int endTime;
 
 }
