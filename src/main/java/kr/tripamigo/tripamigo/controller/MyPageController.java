e2249d31d5f4384a8628d09d537094d4e77307e2
tico <lours2021@gmail.com>
2021-03-31 17:18:03 +0900

    수정: mypage controller 정리

--- src/main/java/kr/tripamigo/tripamigo/controller/MyPageController.java
+++ src/main/java/kr/tripamigo/tripamigo/controller/MyPageController.java
@@ -1,30 +1,26 @@
 package kr.tripamigo.tripamigo.controller;
 
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import kr.tripamigo.tripamigo.domain.Diary;
+import kr.tripamigo.tripamigo.domain.board.Board;
+import kr.tripamigo.tripamigo.domain.board.Info;
+import kr.tripamigo.tripamigo.domain.board.Plan;
+import kr.tripamigo.tripamigo.dto.BoardType;
 import kr.tripamigo.tripamigo.service.DiaryService;
+import kr.tripamigo.tripamigo.service.InfoService;
 import kr.tripamigo.tripamigo.service.PlanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 import kr.tripamigo.tripamigo.domain.User;
 import kr.tripamigo.tripamigo.domain.board.Magazine;
-import kr.tripamigo.tripamigo.dto.DiaryFormDTO;
 import kr.tripamigo.tripamigo.dto.UserFormDTO;
-import kr.tripamigo.tripamigo.exception.LoginException;
 import kr.tripamigo.tripamigo.service.BoardService;
 
 @Controller
@@ -35,97 +31,79 @@ public class MyPageController {
 	private final BoardService boardService;
 	private final PlanService planService;
 	private final DiaryService diaryService;
+	private final InfoService infoService;
+
+	private static User getLoginUser(HttpSession session) {
+		return (User) session.getAttribute("loginUser");
+	}
 
 	@RequestMapping("/home")
     String home(Model model, HttpSession session) {
-
-		User loginUser = (User) session.getAttribute("loginUser");
+		User loginUser = getLoginUser(session);
 		List<Diary> diaryList = diaryService.getDiaryList(loginUser);
+
 		model.addAttribute("diaryList", diaryList);
 
         return "mypage/home";
     }
 
-	@RequestMapping("/diary")
-    String diary(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
-        return "mypage/diary";
-    }
-	@GetMapping("/diaryForm")
-    public String diaryForm(DiaryFormDTO diaryFormDTO, Model model) {
-        return "mypage/diaryForm";
-    }
-	
-	
-	@PostMapping("/diaryForm")
-	public String diaryWrite(@ModelAttribute @Valid DiaryFormDTO diaryFormDTO, BindingResult bindingResult
-			,@RequestPart MultipartFile thumbnail, HttpServletRequest request, HttpSession session, Model model) {
-		if (bindingResult.hasErrors()) {
-			System.out.println(bindingResult.getFieldError());
-			return "mypage/diaryForm";
-		}
-		User user = (User) session.getAttribute("loginUser");
-		if (user == null) {
-			throw new LoginException("로그인하세요", "/login");
-		}
-		//diaryService.writeDiary(diaryFormDTO, user);
-		throw new LoginException("글쓰기 완료", "diary");
-	}
 	@RequestMapping("/plan")
     String plan(Model model, HttpSession session) {
-		User loginUser = (User) session.getAttribute("loginUser");
+		User loginUser = getLoginUser(session);
+		List<Plan> planList = planService.getMyPlanList(loginUser);
 
-		model.addAttribute("planList", planService.listMyPlan(loginUser));
+		model.addAttribute("planList", planList);
+
         return "mypage/plan";
     }
 	
 	@RequestMapping("/board")
-    String board(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String board(Model model, HttpSession session) {
+		User loginUser = getLoginUser(session);
+		Map<BoardType, List<Board>> boardList = boardService.boardList();
+		List<Plan> planList = planService.getMyPlanList(loginUser);
+		List<Info> infoList = infoService.infoList();
+
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("planList", planList);
+		model.addAttribute("infoList", infoList);
+
         return "mypage/board";
     }
 	
-	@RequestMapping("/favorite")
-    String favorite(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
-        return "mypage/favorite";
+	@RequestMapping("/scrap")
+    String favorite(Model model) {
+
+        return "mypage/scrab";
     }
 	
 	@RequestMapping("/note")
-    String note(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String note(Model model) {
+
         return "mypage/note";
     }
 	
 	@RequestMapping("/alarm")
-    String alarm(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String alarm(Model model) {
+
         return "mypage/alarm";
     }
 	
 	@RequestMapping("/followingview")
-    String followingview(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String followingview(Model model) {
+
         return "mypage/followingview";
     }
 	
 	@RequestMapping("/followerview")
-    String followerview(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String followerview(Model model) {
+
         return "mypage/followerview";
     }
 	
 	@RequestMapping("/comment")
-    String comment(UserFormDTO userFormDTO, Model model) {
-		List<Magazine> magazineList = boardService.magazineList();
-    	model.addAttribute(magazineList);
+    String comment(Model model) {
+
         return "mypage/comment";
     }
 
