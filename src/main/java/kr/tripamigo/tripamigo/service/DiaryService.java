2fc170316d9584a680dc686f02319090c4f80579
tico <lours2021@gmail.com>
2021-03-31 17:06:45 +0900

    추가: service diary list method

--- src/main/java/kr/tripamigo/tripamigo/service/DiaryService.java
+++ src/main/java/kr/tripamigo/tripamigo/service/DiaryService.java
@@ -10,45 +10,18 @@ import kr.tripamigo.tripamigo.domain.User;
 import kr.tripamigo.tripamigo.dto.DiaryFormDTO;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Transactional
 @RequiredArgsConstructor
 public class DiaryService {
 
     private final DiaryRepository diaryRepository;
     private final DiaryBoardRepository diaryBoardRepository;
-    public void writeDiary(DiaryFormDTO diaryFormDTO, User user) {
-        DiaryBoard diaryBoard = new DiaryBoard();
-        diaryBoard.setDiarySubject(diaryFormDTO.getSubject());
-        diaryBoard.setDiaryContent(diaryFormDTO.getContent());
-        //diaryBoard.setDiaryThumbnail(diaryFormDTO.getThumbnail());
-//        diaryRepository.save(diaryBoard);
-    }
 
-//    public List<Diary> diaryList(){
-//    	List<Diary> diaryList;
-//    	diaryList = diaryRepository.findAllByBoardStatus(true);
-//    	return diaryList;
-//    }
-//
-//    public Diary readdiary(Long boardSeq) {
-//    	Diary diary = diaryRepository
-//                .findById(boardSeq)
-//                .orElseGet(() -> {
-//                    throw new NoPageException(messageSource
-//                    .getMessage("error.404", null, Locale.getDefault()), "/mypage");
-//                });
-//
-//    	return diary;
-//
-//
-//    }
-//
-//	public void delete(Board board) {
-//		boardRepository.save(board);
-//	}
-//
-//	public void boardHitsUp(Long boardSeq) {
-//		boardRepository.boardHitsUp(boardSeq);
-//	}
+    public List<Diary> getDiaryList(User user) {
+
+        return diaryRepository.findAllByUserAndStatusOrderBySeq(user, true);
+    }
 
 }
