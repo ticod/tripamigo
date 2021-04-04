b19593aa35f02ce4b08736232f36b6354a8982d9
tico <lours2021@gmail.com>
2021-04-05 01:15:18 +0900

    수정: calendar 연도, 월 변경 이벤트 추가

--- src/main/resources/static/script/calendar.js
+++ src/main/resources/static/script/calendar.js
@@ -1,9 +1,50 @@
 export class Calendar {
-    constructor() {
-        this.calendar = document.getElementById("calendar");
-        this.calendarValue = document.getElementById("calendar_value");
 
-        this.createCalendar(2021, 4);
+    constructor() {
+        this.calendar = document.getElementById("calendar_value");
+
+        this.selectYear = document.getElementById("cal-year");
+        this.selectMonth = document.getElementById("cal-month");
+
+        const now = new Date();
+
+        this.years = this.range(now.getFullYear(), now.getFullYear() + 1);
+        this.months = this.range(1, 12);
+
+        this.valueAppendChildTo(this.selectYear, this.years);
+        this.valueAppendChildTo(this.selectMonth, this.months);
+
+        this.selectYear.addEventListener('change', this.change.bind(this));
+        this.selectMonth.addEventListener('change', this.change.bind(this));
+        this.createCalendar(now.getFullYear(), now.getMonth() + 1);
+    }
+
+    range(start, end) {
+        return Array(end - start + 1).fill(start).map((x, y) => x + y);
+    }
+
+    valueAppendChildTo(target, values) {
+        target.options.length = 1;
+        for (const value in values) {
+            if(values.hasOwnProperty(value)) {
+                const option = document.createElement("option");
+                option.text = values[value].toString();
+                option.value = values[value].toString();
+                target.appendChild(option)
+            }
+        }
+    }
+
+    change() {
+        console.log(this.selectMonth.value);
+        if (this.selectYear.value !== "" && this.selectMonth.value !== "") {
+            this.changeCalendar(this.selectYear.value, this.selectMonth.value)
+        }
+    }
+
+    changeCalendar(year, month) {
+        this.calendar.innerHTML = "";
+        this.createCalendar(year, month)
     }
 
     createCalendar(year, month) {
@@ -13,26 +54,40 @@ export class Calendar {
 
         let day = 1;
         let row = document.createElement("div");
+        row.classList.add("cal-row");
 
         for (let i = 1; i <= total; i++) {
-            row.classList.add("cal-row");
-            if (i % 7 === 1) {
-                this.calendarValue.appendChild(row);
+            if (i % 7 === 1 && i !== 1) {
+                this.calendar.appendChild(row);
                 row = document.createElement("div");
+                row.classList.add("cal-row");
             }
 
-            const col = document.createElement("span");
-            col.classList.add("cal_col");
+            const col = document.createElement("div");
+            col.classList.add("cal-col");
             if (i % 7 === 0) {
                 col.classList.add("w3-blue");
             } else if (i % 7 === 1) {
                 col.classList.add("w3-red");
             }
-            const p = document.createElement("p");
 
+            const p = document.createElement("p");
             p.innerHTML = (i <= date.getDay() || day > lastDay.getDate()) ? `&nbsp;` : `${day++}`;
+            p.classList.add("cal-day");
+
+            const weather = document.createElement("div");
+            weather.classList.add("cal-weather");
+
+            if (p.innerHTML !== `&nbsp;`) {
+                weather.innerHTML = "❔";
+            }
+            // 날씨 이모지 : ☀️🌥️🌦️🌪️🌫️☂️⚡⛄
+
             col.appendChild(p);
+            col.appendChild(weather);
             row.appendChild(col);
         }
+        this.calendar.appendChild(row);
     }
+
 }
